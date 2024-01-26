3a6efbff5cc3c8d63584cdd551a64fa9615be648
mehrdadbn9 <mehrdadbiukian@gmail.com>
2024-01-26 19:42:53 +0330

    dockerize project and shell script for db and backup

---
+++ docker/local/postgres/maintenance/_sourced/constants.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# it is recommended to use #!/usr/bin/env bash as it allows for more flexibility in finding the correct interpreter. It ensures that the script will work even if the bash interpreter is located in a different directory or if different systems have a different path to the bash interpreter
+
+BACKUP_DIR_PATH='/backups'
+BACKUP_FILE_PREFIX='backup'
